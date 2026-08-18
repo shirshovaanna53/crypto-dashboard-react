a77bd88270c6117d517164f6c0d3262f746f8a18
Anna Shirshova <shirshova.a@numdes.com>
2026-08-18 15:53:27 +1000

    store, coin gecko api

---
+++ src/services/coinGeckoApi.ts
@@ -0,0 +1,24 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export interface Coin {
+  id: string;
+  symbol: string;
+  name: string;
+  image: string;
+  current_price: number;
+  price_change_percentage_24h: number;
+  market_cap: number;
+}
+
+export const coinGeckoApi = createApi({
+  reducerPath: 'coinGeckoApi',
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://api.coingecko.com/api/v3/' }),
+  endpoints: (builder) => ({
+    getTopCoins: builder.query<Coin[], void>({
+      query: () =>
+        'coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1',
+    }),
+  }),
+});
+
+export const { useGetTopCoinsQuery } = coinGeckoApi;
