f967580cf687c6ca092645018d88f89fa9663464
Anna Shirshova <shirshova.a@numdes.com>
2026-08-19 10:46:34 +1000

    polling, flashing

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,15 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { MantineProvider } from '@mantine/core';
 import { store } from './store';
 import App from './App.tsx';
 
-//import '@mantine/core/styles.css';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <Provider store={store}>
-      <MantineProvider>
-        <App />
-      </MantineProvider>
-    </Provider>
-  </StrictMode>,
+  <Provider store={store}>
+    <MantineProvider>
+      <App />
+    </MantineProvider>
+  </Provider>,
 );
